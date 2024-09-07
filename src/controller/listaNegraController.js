4af2e0580bd580468b9235f2e72c07af9c1c6f6e
macedorayc <macedorayane335@gmail.com>
2024-09-06 23:47:11 -0300

    pronto

---
+++ src/controller/listaNegraController.js
@@ -0,0 +1,95 @@
+import * as db from '../repository/listaNegraRepository.js';
+
+import { Router } from 'express';
+const endpoints = Router();
+
+endpoints.get('/listaNegra/', async (req, resp) => {
+    try {
+
+        let registros = await db.consultarListaNegra();
+
+        resp.send(registros);
+
+    } catch (err) {
+
+        resp.status(400).send({
+            erro: err.message
+        })
+
+    }
+});
+
+endpoints.post('/listaNegra/', async (req, resp) => {
+    try {
+
+        let pessoa = req.body;
+
+        let id = await db.inserirListaNegra(pessoa);
+        
+        resp.send({
+            novoId: id
+        })
+
+    } catch (err) {
+
+        resp.status(400).send({
+            erro: err.message
+        })
+
+    }
+});
+
+endpoints.put('/listaNegra/:id', async (req, resp) => {
+    try {
+
+        let id = req.params.id;
+
+        let pessoa = req.body;
+
+        let linhasAfetadas = await db.alterarListanegra(id, pessoa);
+        if (linhasAfetadas => 1) {
+
+            resp.send();
+
+        } else {
+
+            resp.status(404).send({erro: `Nenhum registro encontrado.`}) 
+
+        }
+
+    } catch (err) {
+
+        resp.status(400).send({
+
+            erro: err.message
+
+        })
+
+    }
+});
+
+endpoints.delete('/listaNegra/:id', async (req, resp) => {
+    try {
+
+        let id = req.params.id;
+
+
+        let linhasAfetadas = await db.removerListaNegra(id);
+        if (linhasAfetadas => 1) {
+
+            resp.send();
+
+        } else {
+
+            resp.status(404).send({erro: `Nenhum registro encontrado.`}) 
+
+        }
+    } catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+});
+
+
+export default endpoints;
